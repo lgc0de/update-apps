be5cbd954f82a03ffa7964d98ad04f8bc3fabbd2
Ludovic G <dev@ludovicgeneix.fr>
2021-12-21 15:28:35 +0100

    fix wrong path qogir

--- themes/theme-qogir.sh
+++ themes/theme-qogir.sh
@@ -16,15 +16,15 @@ if [ ! -d $dir/Qogir ]; then
 fi
 
 # Téléchargement du thème gtk depuis git si sources absentes
-if [ ! -d $dir/Qogir/Qogir-theme ]; then
-    cd $dir/Qogir
+if [ ! -d $dir/themes/Qogir/Qogir-theme ]; then
+    cd $dir/themes/Qogir
     git clone https://github.com/vinceliuice/Qogir-theme.git
     cd Qogir-theme
     install_theme
 fi
 
 # Vérification des mises à jour du thème gtk depuis git
-cd $dir/Qogir/Qogir-theme/
+cd $dir/themes/Qogir/Qogir-theme/
 vargit=$(git pull)
 # gnome + gdm variant
 # sudo ./install.sh -g -c dark -t default -l manjaro -w square
@@ -36,15 +36,15 @@ else
 fi
 
 # Téléchargement du thème d'icone depuis git si sources absentes
-if [ ! -d $dir/Qogir/Qogir-icon-theme ]; then
-    cd $dir/Qogir
+if [ ! -d $dir/themes/Qogir/Qogir-icon-theme ]; then
+    cd $dir/themes/Qogir
     git clone https://github.com/vinceliuice/Qogir-icon-theme.git
     cd Qogir-icon-theme
     install_icon
 fi
 
 # Vérification des mises à jour du thème d'icone depuis git
-cd $dir/Qogir/Qogir-icon-theme
+cd $dir/themes/Qogir/Qogir-icon-theme
 vargit=$(git pull)
 if [ "$vargit" != "Déjà à jour." ]; then
     install_icon
